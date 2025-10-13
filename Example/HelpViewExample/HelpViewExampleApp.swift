e9583c30b95dfb180c89dae3f2d3f3cf475244e0
Amir Hayek <free4sale@gmail.com>
2025-10-13 14:22:34 +0300

    added help to the menu as well

--- Example/HelpViewExample/HelpViewExampleApp.swift
+++ Example/HelpViewExample/HelpViewExampleApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import HelpView
 
 @main
 struct HelpViewExampleApp: App {
+    @Environment(\.openWindow) private var openWindow
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+
+        Window("HelpViewExample Help", id: "help") {
+            NavigationStack {
+                HelpContentView(named: "app_help")
+            }
+        }
+        .commands {
+            CommandGroup(replacing: .help) {
+                Button("HelpViewExample Help") {
+                    openWindow(id: "help")
+                }
+                .keyboardShortcut("?", modifiers: .command)
+            }
+        }
+        .defaultSize(width: 600, height: 700)
     }
 }
